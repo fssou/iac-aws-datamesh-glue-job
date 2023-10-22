86755024c6c3b06be953674f8e8beff0da753fb3
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2023-10-22 08:37:22 -0300

    feat: add permissao para job acessar glue

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,12 +1,8 @@
+
 resource "aws_iam_role" "glue_job" {
   path               = "/iamsr/"
   name               = var.gh_repo_name
   assume_role_policy = data.aws_iam_policy_document.glue_job_assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "glue_job_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-  role       = aws_iam_role.glue_job.name
 }
 
 data "aws_iam_policy_document" "glue_job_assume_role" {
@@ -21,5 +17,33 @@ data "aws_iam_policy_document" "glue_job_assume_role" {
         "glue.amazonaws.com"
       ]
     }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "glue_job_policy" {
+  role       = aws_iam_role.glue_job.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "glue_job_policy" {
+  role       = aws_iam_role.glue_job.name
+  policy_arn = aws_iam_policy.glue_job_permissions.arn
+}
+
+resource "aws_iam_policy" "glue_job_permissions" {
+  path = "/iamsr/"
+  name = var.gh_repo_name
+  policy = data.aws_iam_policy_document.glue_job_permissions.json
+}
+
+data "aws_iam_policy_document" "glue_job_permissions" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "glue:*"
+    ]
+    resources = [
+      "*"
+    ]
   }
 }
