4a1141513002027d5d05093b43cd2cb8e1e77b49
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2023-10-22 10:04:38 -0300

    feat: ajusta security group

--- infra/security.tf
+++ infra/security.tf
@@ -4,10 +4,21 @@ resource "aws_security_group" "main" {
   description = "Security group for ${var.gh_repo_name}"
   vpc_id      = data.aws_vpc.default.id
 
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      data.aws_vpc.default.cidr_block
+    ]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [data.aws_vpc.default.cidr_block]
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
   }
 }
