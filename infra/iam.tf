02207af82812057d76ba316a3983358810809c08
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2023-10-22 09:33:56 -0300

    feat: add permissão de serviço para glue job

--- infra/iam.tf
+++ infra/iam.tf
@@ -25,6 +25,11 @@ resource "aws_iam_role_policy_attachment" "glue_job_s3_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
+resource "aws_iam_role_policy_attachment" "glue_job_s3_access" {
+  role       = aws_iam_role.glue_job.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
 resource "aws_iam_role_policy_attachment" "glue_job_permissions" {
   role       = aws_iam_role.glue_job.name
   policy_arn = aws_iam_policy.glue_job_permissions.arn
