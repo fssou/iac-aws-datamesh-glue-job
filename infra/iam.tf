9c1aa14969eea8dd16a8fd3bf4e1172111194970
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2023-10-22 08:42:02 -0300

    fix: corrige nome de recurso duplicado

--- infra/iam.tf
+++ infra/iam.tf
@@ -20,12 +20,12 @@ data "aws_iam_policy_document" "glue_job_assume_role" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "glue_job_policy" {
+resource "aws_iam_role_policy_attachment" "glue_job_s3_access" {
   role       = aws_iam_role.glue_job.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "glue_job_policy" {
+resource "aws_iam_role_policy_attachment" "glue_job_permissions" {
   role       = aws_iam_role.glue_job.name
   policy_arn = aws_iam_policy.glue_job_permissions.arn
 }
